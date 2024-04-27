39ee015ac34f7ae39c2d89cf7cce52453c3c146a
Roihan Sori <66893648+roihan12@users.noreply.github.com>
2024-04-28 03:47:41 +0700

    Update vite.config.js
    update api host

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     // Get rid of the CORS error
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://thread-api.roihansori.my.id",
         changeOrigin: true,
         secure: false,
       },
